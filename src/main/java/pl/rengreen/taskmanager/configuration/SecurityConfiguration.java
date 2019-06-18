535227cae0a17441b36cdeedfaf70a24e0889453
rengreen <r.m.gardzinska@gmail.com>
2019-06-18 14:58:26 +0200

    Change security configuration to allow acces to h2-console

--- src/main/java/pl/rengreen/taskmanager/configuration/SecurityConfiguration.java
+++ src/main/java/pl/rengreen/taskmanager/configuration/SecurityConfiguration.java
@@ -11,6 +11,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
 }
