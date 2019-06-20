f6b14bf125d8a143793eb5cdc4572a7f0f35a2c9
rengreen <r.m.gardzinska@gmail.com>
2019-06-20 23:02:39 +0200

    Add methods for creating tasks

--- src/main/java/pl/rengreen/taskmanager/controller/TaskController.java
+++ src/main/java/pl/rengreen/taskmanager/controller/TaskController.java
@@ -3,17 +3,26 @@ package pl.rengreen.taskmanager.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.rengreen.taskmanager.model.Task;
+import pl.rengreen.taskmanager.model.User;
 import pl.rengreen.taskmanager.service.TaskService;
+import pl.rengreen.taskmanager.service.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 public class TaskController {
 
     private TaskService taskService;
+    private UserService userService;
 
     @Autowired
-    public TaskController(TaskService taskService) {
+    public TaskController(TaskService taskService, UserService userService) {
         this.taskService = taskService;
+        this.userService = userService;
     }
 
     @GetMapping("/tasks")
@@ -21,6 +30,26 @@ public class TaskController {
 
         model.addAttribute("tasks", taskService.findAll());
         return "views/tasksList";
+    }
+
+    @GetMapping("task/create")
+    public String showTaskForm(Model model) {
+
+        User user = userService.getUserByEmail("admin@mail.com");
+        Task task = new Task();
+        task.setCreator(user);
+        model.addAttribute("task", task);
+        return "views/emptyTaskForm";
+    }
+
+    @PostMapping("task/create")
+    public String createTask(@Valid Task task, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "views/emptyTaskForm";
+        }
+        taskService.createTask(task);
+
+        return "redirect:/tasks";
     }
 
 }
