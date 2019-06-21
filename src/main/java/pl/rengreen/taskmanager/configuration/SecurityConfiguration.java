c0ae233e7be13fabe8f6ef5676d9ac4818336b0e
rengreen <r.m.gardzinska@gmail.com>
2019-06-21 20:58:26 +0200

    Add password encoder and security configuration for autenthication & autorization

--- src/main/java/pl/rengreen/taskmanager/configuration/SecurityConfiguration.java
+++ src/main/java/pl/rengreen/taskmanager/configuration/SecurityConfiguration.java
@@ -1,16 +1,58 @@
 package pl.rengreen.taskmanager.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final DataSource dataSource;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public SecurityConfiguration(BCryptPasswordEncoder bCryptPasswordEncoder, DataSource dataSource) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.
+                jdbcAuthentication()
+                .usersByUsernameQuery("select email as principal, password as credentials, true from user where email=?")
+                .authoritiesByUsernameQuery("select u.email as principal, r.role as role from user u inner join user_role ur on(u.user_id=ur.user_id) inner join role r on(ur.role_id=r.role_id) where u.email=?")
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .rolePrefix("ROLE_");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").permitAll();
+        http
+                .authorizeRequests()
+                .antMatchers("/register", "/", "/login", "/about", "/css/**", "/webjars/**")
+                .permitAll()
+
+                .antMatchers("/profile", "/tasks", "/task/**", "/users")
+                .hasAnyRole("USER, ADMIN")
+
+                .antMatchers("/assign")
+                .hasAnyRole("ADMIN")
+
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/profile")
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login");
 
         http.csrf().disable();
         http.headers().frameOptions().disable();
--- src/main/java/pl/rengreen/taskmanager/service/UserServiceImpl.java
+++ src/main/java/pl/rengreen/taskmanager/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package pl.rengreen.taskmanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.rengreen.taskmanager.model.Role;
 import pl.rengreen.taskmanager.model.User;
@@ -13,16 +14,20 @@ import java.util.*;
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Autowired
     public UserServiceImpl(UserRepository userRepository,
-                       RoleRepository roleRepository) {
+                           RoleRepository roleRepository,
+                           BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @Override
     public User createUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setActive(1);
         Role userRole = roleRepository.findByRole("USER");
         user.setRoles(new ArrayList<>(Collections.singletonList(userRole)));
