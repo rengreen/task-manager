b4ae55a6ec94728bec12f5d8caa637020a17fa89
rengreen <r.m.gardzinska@gmail.com>
2019-08-24 10:38:06 +0200

    Add unit tests for static pages

---
+++ src/test/java/pl/rengreen/taskmanager/controller/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package pl.rengreen.taskmanager.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+public class IndexControllerTest {
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup(){
+        IndexController indexController = new IndexController();
+        mockMvc= MockMvcBuilders.standaloneSetup(indexController).build();
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
---
+++ src/test/java/pl/rengreen/taskmanager/controller/StaticPageControllerTest.java
@@ -0,0 +1,28 @@
+package pl.rengreen.taskmanager.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+public class StaticPageControllerTest {
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup(){
+        StaticPageController staticPageController = new StaticPageController();
+        mockMvc= MockMvcBuilders.standaloneSetup(staticPageController).build();
+    }
+
+    @Test
+    public void testStaticPage() throws Exception {
+        mockMvc.perform(get("/about"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("views/about"));
+    }
+}
