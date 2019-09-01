301ede839be5ca463e01f2c138c114ff36fb1a5c
rengreen <r.m.gardzinska@gmail.com>
2019-09-01 11:17:55 +0200

    Add comments about user login

--- src/main/java/pl/rengreen/taskmanager/controller/LoginController.java
+++ src/main/java/pl/rengreen/taskmanager/controller/LoginController.java
@@ -1,13 +1,14 @@
 package pl.rengreen.taskmanager.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
+    @GetMapping("/login")
     String showLoginForm() {
+        //login form is submitted using POST method <form th:action="@{/login}" method="post">
         return "views/loginForm";
     }
 }
